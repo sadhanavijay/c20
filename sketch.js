fb0cc55e9e2edb2aa69572b329832d0dacee56fe
sadhanavijay <85877153+sadhanavijay@users.noreply.github.com>
2021-07-30 15:56:16 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -26,20 +26,6 @@ function setup() {
 }
 function draw() {
   background("space"); 
- 
-  textSize(20);
-  fill("black") ;
-  text("Instruction",20,35);
-  textSize(15);
-  text("0=sleeping",20,55);
-  text("1=brushing",20,75);
-  text("2=bathing",20,90);
-  text("3=exercise",20,105);
-  text("4=eating",20,130);
-  text("5=drinking",20,155);
-  text("6=movinging",20,175);
- text.depth=space.depth+1
-
 
   if(keyDown("0")){
     astronaut.addAnimation("sleeping",sleep);
@@ -93,4 +79,19 @@ function draw() {
   }
   
   drawSprites();
+  
+  
+  textSize(20);
+  fill("black") ;
+  text("Instruction",20,35);
+  textSize(15);
+  text("0=sleeping",20,55);
+  text("1=brushing",20,75);
+  text("2=bathing",20,90);
+  text("3=exercise",20,105);
+  text("4=eating",20,130);
+  text("5=drinking",20,155);
+  text("6=movinging",20,175);
+ text.depth=space.depth+1
+
 }
